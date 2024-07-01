bd7ab6d5030a49d2167f24c4925e61818c369d0d
pragati-dhawale <you@example.com>
2024-07-01 15:39:35 +0530

    Modified Q4

--- Q4.js
+++ Q4.js
@@ -1,22 +1,22 @@
-// Given a string, find the length of the longest substring without repeating characters.
-let str="abcbca"
+// // Given a string, find the length of the longest substring without repeating characters.
+// let str="abcbca"
 
-function longestSubstring(){
-    let s=str[0],maxC=0,l=0
-    //  console.log(str.indexOf(k)==-1)
-    for (let i=0; i<str.length; i++){
-        for (let j=i+1; j<str.length; j++){
-            if (!s.includes(s[j])){
-                s=s+str[j];
-                // console.log(s)
-            }
-            else {
+let str = "abcbcadefg";
+
+function longestSubstring() {
+    let s = ""; 
+
+    for (let i = 0; i < str.length; i++) {
+        for (let j = i + 1; j < str.length; j++) {
+            if (!s.includes(str[j])) { 
+                s = s + str[j]; 
+            } else { 
+                break; 
                 return s.length;
             }
-
         }
     }
-
+    return s.length; 
 }
 
 console.log(longestSubstring(str));
